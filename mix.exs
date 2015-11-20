18b9ec6cc7172e4d0db5f012895c709ec0b9c3cc
Taylor Brooks <tbrooks@gmail.com>
2015-11-20 16:12:21 -0600

    Bump to 0.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExCloseio.Mixfile do
 
   def project do
     [app: :ex_closeio,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
