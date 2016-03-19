50b6bb7491a2c3f11755da3114707bea00c724ba
Taylor Brooks <tbrooks@gmail.com>
2016-03-19 01:45:09 -0500

    Hanging slash; Bump to 0.0.10

--- lib/ex_closeio/resources/report.ex
+++ lib/ex_closeio/resources/report.ex
@@ -3,12 +3,12 @@ defmodule ExCloseio.Report do
 
   # OPTIONS [date_start, date_end, user_id]
   def activities(params, api_key \\ :global) do
-    get("/report/activity/#{get_org_id}", api_key, params)
+    get("/report/activity/#{get_org_id}/", api_key, params)
   end
 
   # OPTIONS [date_start, date_end, user_id]
   def sent_emails(params, api_key \\ :global) do
-    get("/report/sent_email/#{get_org_id}", api_key, params)
+    get("/report/sent_email/#{get_org_id}/", api_key, params)
   end
 
   defp get_org_id do
